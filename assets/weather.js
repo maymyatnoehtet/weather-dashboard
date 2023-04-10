d90776d445fabe522b2e5591ebdd92faa2354263
maymyatnoehtet <may.mn.htet@gmail.com>
2023-04-10 19:43:33 +1000

    fetch data from server

--- assets/weather.js
+++ assets/weather.js
@@ -1,8 +1,17 @@
-/* will fetch the data from OpenWeather api */
-apikey = 'ff6b3036415b6c713a948b9abb79f243';
-url = "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}";
+/* Will use OpenWeather API to retrieve the weather data of multiple cities */
+const apiKey = 'ff6b3036415b6c713a948b9abb79f243';
+var lat = 34.6937;
+var lon = 135.5023;
+const forecastContainer = document.querySelector('.forecast');
 
-/* request header */
-requestHeader = {
-    method: "GET"
-}
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then(response => {
+        console.log(response);
+        response.json();
+    })
+    .then(data => {
+        console.log(typeof data);
+    })
+    .catch(error => {
+        console.log('Error fetching data:', error);
+    });
