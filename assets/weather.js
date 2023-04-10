6037d9964e182284845fe49d43f9a32d8abce102
maymyatnoehtet <may.mn.htet@gmail.com>
2023-04-11 09:10:35 +1000

    get the coordinate of the chosen city

--- assets/weather.js
+++ assets/weather.js
@@ -1,17 +1,26 @@
-/* Will use OpenWeather API to retrieve the weather data of multiple cities */
-const apiKey = 'ff6b3036415b6c713a948b9abb79f243';
-var lat = 34.6937;
-var lon = 135.5023;
-const forecastContainer = document.querySelector('.forecast');
+/* all the global variables */
+var city = "London";
+const apikey = 'ff6b3036415b6c713a948b9abb79f243';
+var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apikey}`;
+var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longtitude}&units=metric&appid=${apikey}`;
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-    .then(response => {
-        console.log(response);
-        response.json();
-    })
-    .then(data => {
-        console.log(typeof data);
-    })
-    .catch(error => {
-        console.log('Error fetching data:', error);
-    });
+/* to get the latitude and longtitude of the city  */
+function getCoords(){
+    console.log("GeoURL: " + geoUrl); // TODO: delete this before submitting
+    fetch(geoUrl)
+        .then(response => response.json())
+        .then(data => extractLatLon(data))
+        .catch(error => console.log('error: ', error));
+};
+
+getCoords(city);
+
+
+/* to extract latitude and longtitude data (used GEOCODING API) */
+function extractLatLon(data){
+    var latitude = data[0].lat;
+    var longtitude = data[0].lon;
+    console.log("Latitude: "+ latitude);
+    console.log("Longtitude: "+ longtitude);
+    return;
+}
