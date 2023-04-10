be1748c1e016a4d210b0e50f216e65d7f99dafdc
maymyatnoehtet <may.mn.htet@gmail.com>
2023-04-11 09:20:50 +1000

    get the weather data from the server

--- assets/weather.js
+++ assets/weather.js
@@ -2,9 +2,8 @@
 var city = "London";
 const apikey = 'ff6b3036415b6c713a948b9abb79f243';
 var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apikey}`;
-var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longtitude}&units=metric&appid=${apikey}`;
 
-/* to get the latitude and longtitude of the city  */
+/* to get the latitude and longtitude of the city (used GEOCODING API) */
 function getCoords(){
     console.log("GeoURL: " + geoUrl); // TODO: delete this before submitting
     fetch(geoUrl)
@@ -22,5 +21,19 @@ function extractLatLon(data){
     var longtitude = data[0].lon;
     console.log("Latitude: "+ latitude);
     console.log("Longtitude: "+ longtitude);
-    return;
+    getForecast(latitude, longtitude);
+}
+
+/* to get the weather forecast data (used OPENWEATHER API) */
+function getForecast(lat, lon) {
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`;
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => renderForecast(data))
+        .catch(error => console.log('error: ', error));
+}
+
+/* to render the weather data retrieved */
+function renderForecast(weatherData){
+
 }
